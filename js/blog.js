3dc388a8fbe848234d072c177f87d52533ab25c0
qingzhui <670761743@qq.com>
2015-09-10 16:15:30 +0800

    更新读取数据

--- js/blog.js
+++ js/blog.js
@@ -3,11 +3,34 @@ $(function(){
 
     Parse.initialize("ZMnzRQkMCF7c0YN57snOmEEGhs2EXLp2LxZhUC2g", "ZndjZJvoTNz5WBSuLSqkEVHvIJkSauyB0Bzb9iX6");
 
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-
-    testObject.save({foo: "bar"}).then(function(object) {
-        alert("yay! it worked");
+    var Blog = Parse.Object.extend("Blog");
+    var Blogs = Parse.Collection.extend({
+        model: Blog
     });
+
+    var blogs = new Blogs();
+
+    var BlogsView =  Parse.View.extend({
+        template: Handlebars.compile($('#blogs-tpl').html()),
+        render: function(){ 
+            var collection = { blog: this.collection.toJSON() };
+            this.$el.html(this.template(collection));
+        }
+    });
+
+    blogs.fetch({
+        success: function(blogs) {
+            console.log(blogs);
+
+            var blogsView = new BlogsView({ collection: blogs });
+            blogsView.render();
+            $('.main-container').html(blogsView.el);
+        },
+        error: function(blogs, error) {
+            console.log(error);
+        }
+    });
+
+    
 
 });
